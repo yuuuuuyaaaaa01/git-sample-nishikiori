b4fc0f5b77efbb47c39e682a52609a5080901333
nishikiori <yuya.nishikiori@rakus.co.jp>
2018-10-30 10:58:46 +0900

    新規にプロジェクトを作成

---
+++ src/main/java/jp/co/sample/GitSampleNishiApplication.java
@@ -0,0 +1,12 @@
+package jp.co.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleNishiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleNishiApplication.class, args);
+	}
+}
