64bf722876b27f5a502e29c723b470fb98b7629f
nishikiori <yuya.nishikiori@rakus.co.jp>
2018-10-30 11:25:50 +0900

    新規にCar を追加。コメントを出力

---
+++ src/main/java/jp/co/sample/Car.java
@@ -0,0 +1,21 @@
+package jp.co.sample;
+
+/**
+ * 
+ *  @author nishiki
+ * **/
+
+public class Car {
+	private Integer speed;
+
+	
+	public Integer getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(Integer speed) {
+		this.speed = speed;
+	}
+	
+	
+}
--- src/main/java/jp/co/sample/GitSampleNishiApplication.java
+++ src/main/java/jp/co/sample/GitSampleNishiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitSampleNishiApplication {
 
 	public static void main(String[] args) {
+		System.out.println("デプロイとサーバー起動を行います");
 		SpringApplication.run(GitSampleNishiApplication.class, args);
 	}
 }
